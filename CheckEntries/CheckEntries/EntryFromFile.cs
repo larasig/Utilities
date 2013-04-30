de344498117d561394f62363a567e023a60adf32
Larasig Svenska <larasig.svenska@gmail.com>
2013-04-30 16:08:16 +0800

    RF list comparison

---
+++ CheckEntries/CheckEntries/EntryFromFile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CheckEntries
+{
+    class EntryFromFile
+    {
+        public String FileName
+        {
+            get;
+            set;
+        }
+
+        internal EntryFromFile(String fileName)
+        {
+            FileName = fileName;
+        }
+
+        internal IEnumerable<String> Lines
+        {
+            get
+            {
+                using (StreamReader sr = new StreamReader(FileName))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        String l = sr.ReadLine();
+                        if (!String.IsNullOrEmpty(l.Trim()) && !l.StartsWith("#"))
+                        {
+                            yield return l;
+                        }
+                    }
+                }
+            }
+        }
+
+        private List<String> _Lines;
+
+        internal List<String> FindMissingEntries(EntryFromFile cmp)
+        {
+            if (_Lines == null)
+            {
+                _Lines = new List<string>();
+                _Lines.AddRange(Lines);
+            }
+            List<String> result = new List<String>();
+            //result.AddRange(cmp.Lines);
+
+            foreach (var s in cmp.Lines)
+            {
+                if (!_Lines.Contains(s))
+                {
+                    result.Add(s);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CheckEntries/CheckEntries/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace CheckEntries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntryFromFile entry1 = new EntryFromFile(@"E:\ext\all_malwareindication.txt");
+            EntryFromFile entry2 = new EntryFromFile(@"E:\ext\malware_type_ngrams.txt");
+            foreach (var e in entry1.FindMissingEntries(entry2))
+            {
+                Trace.WriteLine(e);
+            }
+        }
+
+        
+    }
+}
